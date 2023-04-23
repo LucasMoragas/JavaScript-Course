d1c1cc2bffaf53de1901bdcf4d97f4851e8e211f
LucasMoragas <moragaslucas08@gmail.com>
2023-04-23 17:15:20 -0300

    Módulo 4 teoria
    3 primeiras aulas do módulo 4. O conteúdo estudo foi condições em JavaScript.

---
+++ Módulo 04/Aula 11/ex08.js
@@ -0,0 +1,7 @@
+var pais = 'Espanha'
+
+if(pais == 'Brasil'){
+    console.log('Brasileiro!!')
+}else{
+    console.log('Estrangeiro!!')
+}
---
+++ Módulo 04/Aula 12/ex11.js
@@ -0,0 +1,10 @@
+var idade = 10
+
+
+if((idade >= 16 && idade < 18) || (idade >= 65)){
+    console.log('Voto opcional')
+}else if(idade >= 18){
+    console.log('Voto obrigatório')
+}else{
+    console.log('Não vota')
+}
